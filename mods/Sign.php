3d42473784f59ba8fa631eb3d74996d8b31b8947
Whale <jesse2061@outlook.com>
2019-08-27 16:41:26 +0800

    Update Sign.php

--- mods/Sign.php
+++ mods/Sign.php
@@ -6,26 +6,36 @@
  * Time: 14:35
  */
 
-use DataProvider as DP;
+namespace crazybot\mods;
+
+use crazybot\CrazyBot;
+use crazybot\manager\TimeManager;
+use crazybot\utils\DataProvider as DP;
+use crazybot\utils\ZMUtil;
+use framework\ZMBuf;
 
 class Sign extends ModBase
 {
     public function __construct(CrazyBot $main, $data) {
         parent::__construct($main, $data);
-        $cq = ZMUtil::getCQ($data["message"]);
-        if (($cq !== null && $cq["type"] == "sign") || mb_strpos($data['message'],'群签到') !== false) {
-            $this->doSign();
-        }
     }
 
-    public static function initValues() {
+    public function onMessage($msg) {
+        $cq = ZMUtil::getCQ($msg);
+        if (($cq !== null && $cq["type"] == "sign") || mb_strpos($msg, '群签到') !== false) {
+            $this->doSign();
+        }
+        return true;
+    }
+
+    public function onStart() {
         $sign = DP::getJsonData("Sign_config.json");
         ZMBuf::set("sign_mode", $sign["mode"] ?? true);
         ZMBuf::set("sign_attribute", $sign["attribute"] ?? []);
         ZMBuf::set("sign_first", $sign["first"] ?? []);
     }
 
-    public static function saveValues() {
+    public function onSave() {
         $sign = DP::getJsonData("Sign_config.json");
         $sign["mode"] = ZMBuf::get("sign_mode");
         $sign["attribute"] = ZMBuf::get("sign_attribute");
@@ -33,92 +43,71 @@ class Sign extends ModBase
         DP::setJsonData("Sign_config.json", $sign);
     }
 
-    public function execute($it) {
-        switch ($it[0]) {
-            case "签到":
-                if ($this->getMessageType() == "wechat") {
-                    $this->reply("微信暂不支持此功能哦！");
-                    return true;
-                }
-                if($this->doSign() === false){
-                    $this->reply("你已经签过到啦！");
-                }
-                return true;
-            /*case "签到排行":
-                $users = ZMUtil::getAllUsers(true);
-                $points = [];
-                foreach ($users as $k => $v) {
-                    $points[$k] = $v->getPoint();
-                }
-                arsort($points);
-                $msg = "「积分排行」";
-                $i = 0;
-                foreach ($points as $k => $v) {
-                    if ($i >= 7) break;
-                    $msg .= "\n[" . ($i + 1) . "] " . $k . ": " . $v . "积分";
-                    $i++;
-                }
-                $msg .= "\n===============";
-                $msg .= "\n「连签排行」";
-                $points = [];
-                foreach ($users as $k => $v) {
-                    $points[$k] = $v->getSignDay();
-                }
-                arsort($points);
-                $i = 0;
-                foreach ($points as $k => $v) {
-                    if ($i >= 4) break;
-                    $msg .= "\n[" . ($i + 1) . "] " . $k . ": " . $v . "天";
-                    $i++;
-                }
-                $this->reply($msg);
-                return true;*/
-            case "设置签到":
-                if (!$this->main->isAdmin($this->getUserId())) return true;
-                if (isset($it[1])) {
-                    switch ($it[1]) {
-                        case "开启":
-                            ZMBuf::set("sign_mode", true);
-                            $this->reply("成功开启签到功能！");
-                            return true;
-                        case "关闭":
-                            ZMBuf::set("sign_mode", false);
-                            $this->reply("成功关闭签到功能！");
-                            return true;
-                        case "积分范围":
-                        case "首签加成":
-                            if (count($it) < 4) {
-                                $this->reply("用法：设置签到 " . $it[1] . " min max");
-                                return true;
-                            }
-                            if (!is_numeric($it[2]) || !is_numeric($it[3])) {
-                                $this->reply("请输入数字！");
-                                return true;
-                            }
-                            $min = intval($it[2]);
-                            $max = intval($it[3]);
-                            if ($min > $max) {
-                                $this->reply("请输入从小到大的数字！");
-                                return true;
-                            }
-                            $attribute = ZMBuf::get("sign_attribute");
-                            $attribute[($it[1] == "积分范围" ? "point" : "first")] = [$min, $max];
-                            ZMBuf::set("sign_attribute", $attribute);
-                            $this->reply("成功设置签到" . $it[1] . "为从" . $min . "到" . $max . "!");
-                            return true;
-                    }
-                }
-                $msg = "「签到设置系统帮助」";
-                $msg .= "\n设置签到 积分范围 min max";
-                $msg .= "\n设置签到 开启：开启签到功能";
-                $msg .= "\n设置签到 关闭：关闭签到功能";
-                $msg .= "\n设置签到 首签加成 min max";
-                $this->reply($msg);
-                return true;
+    /**
+     * 签到
+     * @return bool
+     */
+    public function sign() {
+        if ($this->getMessageType() == "wechat") {
+            $this->reply("微信暂不支持此功能哦！");
+            return true;
         }
-        return false;
+        if ($this->doSign() === false) {
+            $this->reply("你已经签过到啦！");
+        }
+        return true;
     }
 
+    /**
+     * 设置签到
+     * @param $arg
+     * @return bool
+     */
+    public function signSettings($arg) {
+        if (!$this->isAdmin($this->getUserId())) return true;
+        if (isset($arg[1])) {
+            switch ($arg[1]) {
+                case "开启":
+                    ZMBuf::set("sign_mode", true);
+                    $this->reply("成功开启签到功能！");
+                    return true;
+                case "关闭":
+                    ZMBuf::set("sign_mode", false);
+                    $this->reply("成功关闭签到功能！");
+                    return true;
+                case "积分范围":
+                case "首签加成":
+                    if (count($arg) < 4) {
+                        $this->reply("用法：设置签到 " . $arg[1] . " min max");
+                        return true;
+                    }
+                    if (!is_numeric($arg[2]) || !is_numeric($arg[3])) {
+                        $this->reply("请输入数字！");
+                        return true;
+                    }
+                    $min = intval($arg[2]);
+                    $max = intval($arg[3]);
+                    if ($min > $max) {
+                        $this->reply("请输入从小到大的数字！");
+                        return true;
+                    }
+                    $attribute = ZMBuf::get("sign_attribute");
+                    $attribute[($arg[1] == "积分范围" ? "point" : "first")] = [$min, $max];
+                    ZMBuf::set("sign_attribute", $attribute);
+                    $this->reply("成功设置签到" . $arg[1] . "为从" . $min . "到" . $max . "!");
+                    return true;
+            }
+        }
+        $msg = "「签到设置系统帮助」";
+        $msg .= "\n设置签到 积分范围 min max";
+        $msg .= "\n设置签到 开启：开启签到功能";
+        $msg .= "\n设置签到 关闭：关闭签到功能";
+        $msg .= "\n设置签到 首签加成 min max";
+        $this->reply($msg);
+        return true;
+    }
+
+//API part
     private function initData() {
         DP::query("INSERT INTO sign_data (user_id, point, sign_day_long, last_sign, sign_count) VALUES (?,?,?,?,?)", [$this->getUserId(), 0, 1, 0, 0]);
         return [
@@ -178,10 +167,6 @@ class Sign extends ModBase
         ]);
         $msg .= "\n累计签到：" . $sign_data["sign_count"] . "次";
         $msg .= "\n当前积分：" . $sign_data["point"];
-        if($sign_data["point"] >= 150) {
-            CQAPI::send_like($this->getConnection(), $this->getUserId());
-            $msg .= "\n炸毛给你一个赞～！";
-        }
         return $this->reply($msg);
     }
 }
